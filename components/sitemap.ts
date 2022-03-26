c5ac97ede38d62706588cf50a3d6d75289b891dd
Shubhan Chemburkar <schemburkar@outlook.com>
2022-03-26 16:48:33 +0530

    Feature sitemaps (#4)
    * added sitemap generation using index

---
+++ components/sitemap.ts
@@ -0,0 +1,36 @@
+import { writeFile } from 'fs/promises'
+import { IItemData } from "../lib/FileFormat";
+
+const url = (url: string, date: string, freq = 'daily') => `
+<url>
+    <loc>${url}</loc>
+    <lastmod>${date}</lastmod>
+    <changefreq>${freq}</changefreq>
+    <priority>1.0</priority>
+</url>
+`;
+
+const siteMapXML = (...urls: string[]) => `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls.join('')}
+</urlset>`;
+
+const getSiteMap = async ([posts, pages]: [IItemData[], IItemData[]]) => {
+    try {
+        const baseUrl = `https://${process.env.VERCEL_URL || `octocat.dev`}`;
+        //const [posts, pages] = await Promise.all([getDataAPIByType("posts").getAllItems(['slug', 'date']), getDataAPIByType("pages").getAllItems(['slug'])]);
+        const postUrls = posts.map(({ slug, type, date }) => url(`${baseUrl}/${type}/${slug}`, (date ? new Date(date) : new Date()).toISOString()));
+        const pagesUrls = pages.map(({ slug, type }) => url(`${baseUrl}/${type}/${slug}`, new Date().toISOString()));
+        const sitemap = siteMapXML(url(`${baseUrl}/`, new Date().toISOString()), pagesUrls.join(""), postUrls.join(""))
+        return sitemap;
+    }
+    catch (e) {
+        return ''
+    }
+};
+
+export const saveSiteMap = async ([posts, pages]: [IItemData[], IItemData[]]) => {
+    // writes sitemap.xml to public directory
+    const path = `${process.cwd()}/public/sitemap.xml`;
+    await writeFile(path, await getSiteMap([posts, pages]), "utf8");
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    images: {
-      domains: ['avatars.githubusercontent.com','assets.vercel.com','worldwidecode.files.wordpress.com','octocat.dev'],
-    },
+  images: {
+    domains: ['avatars.githubusercontent.com', 'assets.vercel.com', 'worldwidecode.files.wordpress.com', 'octocat.dev'],
   }
+}
