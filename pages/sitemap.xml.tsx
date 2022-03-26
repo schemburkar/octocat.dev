8aae3075a62085a087d1e70faef940b4404bc898
Shubhan Chemburkar <schemburkar@outlook.com>
2022-03-26 13:53:37 +0530

    xml updates

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -4,9 +4,7 @@ import { readdir } from 'fs/promises'
 import { GetServerSideProps } from "next";
 import path from 'path'
 import getConfig from 'next/config'
-const { serverRuntimeConfig } = getConfig()
 
-const PROJECT_ROOT = serverRuntimeConfig.PROJECT_ROOT;
 
 const Sitemap = () => { };
 
@@ -14,11 +12,11 @@ const Sitemap = () => { };
 const read = async (path: string, res: any) => {
     try {
 
-        res.write(`<path>${path}<files>`)
-        const files = readdir(path);
+        res.write(`<path>${path}</path>`)
+        const files = await readdir(path);
 
         if (files) {
-            res.write(`<files>${(await files).map(file => file).join('')}<files>`)
+            res.write((files).map(file => `<files>${file}</files>`).join(''))
         }
     }
     catch (e) {
@@ -30,24 +28,30 @@ const read = async (path: string, res: any) => {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     const resp = ''
+    res.setHeader("Content-Type", "text/xml");
+    res.write(`<?xml version="1.0" encoding="UTF-8"?>`)
+    res.write(`<paths>`)
+
     try {
 
-        res.setHeader("Content-Type", "text/xml");
-        read('./.next/server/pages', res);
-        read('./', res);
-        read(PROJECT_ROOT, res);
-        read(path.join(PROJECT_ROOT,'./pages'), res);
+        await read('./.next/server/pages', res);
+        await read('./', res);
+
+        const { serverRuntimeConfig } = getConfig();
+
+        const PROJECT_ROOT = serverRuntimeConfig.PROJECT_ROOT;
+        await read(PROJECT_ROOT, res);
+        await read(path.join(PROJECT_ROOT, './pages'), res);
 
 
-        res.end();
-
-
+        
+        
     }
     catch (e) {
-        res.setHeader("Content-Type", "text/xml");
-        res.write(`<error>${e}</error>`);
-        res.end();
+                res.write(`<error>${e}</error>`);
     }
+    res.write(`</paths>`)
+    res.end();
     return {
         props: {},
     };
