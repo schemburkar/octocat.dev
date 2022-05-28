a1f22154b4624a2806a5d5b8c153bf3b990dd900
Shubhan Chemburkar <schemburkar@outlook.com>
2022-05-28 18:35:40 +0530

    Fix build by adding avatar null check

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 
 type AvatarProps = {
-  name: string
-  picture: string
+  name?: string
+  picture?: string
 }
 export default function Avatar({ name, picture }: AvatarProps) {
+  if (!name || !picture) return null;
+  
   return (
     <div className="flex items-center">
       <div className="w-12 h-12 rounded-full mr-4"><Image src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} width={48} height={48} /></div>
