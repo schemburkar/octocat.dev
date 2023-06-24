7aaf75bb8e59339d08be698b692d9fbb86363dde
Shubhan Chemburkar <schemburkar@outlook.com>
2023-06-24 16:58:12 +0530

    Remove extra lines

--- app/(home)/@modal/(.)quick-search/page.tsx
+++ app/(home)/@modal/(.)quick-search/page.tsx
@@ -7,10 +7,8 @@ const SearchModal = ({ searchParams }: SearchModalProps) => {
     return <>
         <Modal>
             <section className="p-8 bg-white dark:bg-black rounded-lg">
-
                 <article className="p-2">
                     <div className="flex mt-2 items-baseline">
-
                         <h3 className="text-lg grow">{"Quick Search"}</h3>
                         <CloseSearch>
                             <div className='text-right '><a href='#' className="underline hover:text-success duration-200 transition-colors">Close Search</a><span className='text-xs p-1 mx-1 border-gray-300 border rounded text-'>ESC</span></div>
