455cb9fdd7102988e7373db9f10d5cbab9014735
Shubhan Chemburkar <schemburkar@outlook.com>
2023-06-29 08:55:17 +0530

    Packages, Archive Page, removed pages

---
+++ app/(base)/(home)/archive/page.tsx
@@ -0,0 +1,43 @@
+import MoreStories from '../../../../components/more-stories'
+import { getDataAPIByType } from '../../../../lib/data-api'
+import { ArchivePosts } from '../../../../lib/constants'
+import { IItemData } from '../../../../lib/FileFormat'
+
+type PageProps = { archivePosts: IItemData[] }
+const Index = async () => {
+
+    const { archivePosts } = await getStaticProps()
+    return (<>
+        <>
+
+            <section>
+                {archivePosts.length > 0 && <MoreStories title={ArchivePosts} posts={archivePosts} />}
+            </section>
+
+        </>
+    </>
+    )
+}
+
+export default Index;
+
+const getStaticProps = async (): Promise<PageProps> => {
+
+    const allPosts = await getDataAPIByType("posts").getAllItems([
+        'title',
+        'date',
+        'slug',
+        'author',
+        'coverImage',
+        'excerpt',
+        'isHeroPost',
+        'coverImageAspectRatio',
+        'isArchive'
+    ]);
+
+    const archivePosts = allPosts.filter(a => a.isArchive === true);
+
+    return { archivePosts }
+}
+
+
--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/index.scss'
-import { AppProps } from 'next/app'
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default App;
